09f16c0e0ae56deb45422cb06a372bbeb043afd6
Owen Healy <owen@pulsetor.com>
2020-11-22 02:00:41 -0500

    Starting to put this together...

--- src/dev_input_mapping.rs
+++ src/dev_input_mapping.rs
@@ -4,10 +4,60 @@
 use nix::fcntl::{open, OFlag};
 use nix::sys::stat::Mode;
 use nix::unistd::{read, write};
+use nix::Error;
 use libc::{input_event};
 use std::mem::size_of;
 use uinput_sys::{ui_set_evbit, EV_SYN, EV_KEY, EV_MSC, ui_dev_create, ui_set_keybit, KEY_MAX, KEY_7};
 use crate::struct_ser::StructSerializer;
+use std::os::unix::io::RawFd;
+use nix::ioctl_write_int;
+
+pub struct TypeCodeValue {
+  pub type_: u16,
+  pub code: u16,
+  pub value: i32,
+}
+
+pub struct DevInputReader {
+  fd: RawFd
+}
+
+impl DevInputReader {
+  pub fn read_next(self: &mut DevInputReader) -> TypeCodeValue {
+    let size = size_of::<input_event>();
+    let mut buf: Vec<u8> = vec![0; size];
+    read(self.fd, &mut buf).unwrap();
+    
+    let type_ = u16::from_ne_bytes([buf[16], buf[17]]);
+    let code = u16::from_ne_bytes([buf[18], buf[19]]);
+    let value = i32::from_ne_bytes([buf[20], buf[21], buf[22], buf[23]]);
+    
+    TypeCodeValue {
+      type_: type_,
+      code: code,
+      value: value
+    }
+  }
+  
+  pub fn open(path: &str, exclusive: bool) -> Result<DevInputReader, Error> {
+    let fd = open(path, OFlag::O_RDONLY, Mode::empty())?;
+    
+    if exclusive {
+      unsafe {
+        eviocgrab(fd, 1)?;
+      }
+    }
+    
+    Ok(DevInputReader {
+      fd: fd
+    })
+  }
+}
+
+const EVIOCGRAB_NUM: u8 = b'E';
+const EVIOCGRAB_SEQ: u8 = 0x90;
+
+ioctl_write_int!(eviocgrab, EVIOCGRAB_NUM, EVIOCGRAB_SEQ);
 
 pub fn read_test() {
   println!("OK!");
